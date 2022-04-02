8b12774be0f43d58bcafc7bb3dc35f8f427bb6d3
lybcyd <lybcyd@outlook.com>
2022-04-02 17:53:55 +0800

    显示个人资料

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -9,10 +9,10 @@
           alt="{{ $user->name }}">
         <div class="grow shrink p-4">
           <h5 class="text-xl mb-2"><strong>个人简介</strong></h5>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+          <p>{{ $user->introduction }}</p>
           <hr class="my-4">
           <h5 class="text-xl mb-2"><strong>注册于</strong></h5>
-          <p>January 01 1901</p>
+          <p>{{ $user->created_at->diffForHumans() }}</p>
         </div>
       </div>
     </div>
